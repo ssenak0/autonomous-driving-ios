cf86f022ad61d456c8233d65c30bb7484a7f9837
Sena <sena250817@gmail.com>
2026-03-25 20:01:02 +0300

    Initial Commit

---
+++ CarSimulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarSimulator
+//
+//  Created by Sena on 25.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
